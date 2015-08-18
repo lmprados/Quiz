56b7df0e3189485691a91a05e5431b9a92268fa2
Luis M. Prados <lmprados+git@gmail.com>
2015-08-18 15:52:19 -0700

    Aprobacion de comentarios

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,5 +1,19 @@
 var models = require('../models/models.js');
 
+//Autoload - factoriza el codigo si ruta incluye :commentId
+exports.load = function (req, res, next, commentId){
+	models.Comment.find({
+		where: {id: Number(commentId)}
+		
+	}).then (
+		function(comment) {
+			if (comment) {
+				req.comment=comment;
+				next();
+			} else { next (new Error('No existe commentId='+commentId));}
+	}).catch(function(error) {next (error);});
+};
+
 //GET /quizes/:id/comments/new
 exports.new = function(req, res){
 	
@@ -24,6 +38,16 @@ exports.create = function(req, res){
 			}
 	}).catch(function(error){next(error)}); 
 };
+
+// GET /quizes/:quizId/comments/:commentId/publish
+exports.publish = function(req, res) {
+	req.comment.publicado=true;
+
+	req.comment.save({fields: ["publicado"]})
+		.then(function(){res.redirect('/quizes/'+req.params.quizId);})
+		.catch(function(error){next(error)});
+};
+
 
 
 
